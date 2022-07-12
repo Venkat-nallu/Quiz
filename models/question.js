cf98a536d78b306adf47ed7482429aa96c9bbf5e
Venkatesh N <54276932+Venkat-nallu@users.noreply.github.com>
2022-07-12 15:12:36 +0530

    Removed unwanted logs and comments

--- models/question.js
+++ models/question.js
@@ -1,36 +1,3 @@
-// const mongoose1 = require('mongoose');
-
-// const quesSchema = new mongoose1.Schema({
-    
-//     q: 
-//     {
-//         type: String,
-//         required: true
-//     },
-
-//     options: [ { type:String, required: true } ],
-
-//     answer:
-//     {
-//         type: String,
-//         required: true
-//     },
-//     qzName:
-//     {
-//         type:String,
-//         required:true
-//     }
-
-// },{ timestamps: true});
-
-
-
-// const QuestionSchema = mongoose1.model('QuestionSchema', quesSchema);
-
-// module.exports = QuestionSchema;
-
-
-
 const mongoose = require('mongoose');
 
 const QuestionSchema = new mongoose.Schema({
@@ -60,7 +27,6 @@ const QuestionSchema = new mongoose.Schema({
     }
 
 },{ timestamps: true});
-
 
 
 module.exports = QuestionSchema;
